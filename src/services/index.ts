9b4b0cec15dd3479b5fda59ebe7457a630b9b023
0xThresh <0xthresh@protonmail.com>
2022-06-25 19:07:49 -0400

    Added base code, untested

--- src/services/index.ts
+++ src/services/index.ts
@@ -97,3 +97,39 @@ const buildSingleDataEntry = (messages: Array<Message>): DataEntry => {
 //4.
 
 
+// send transaction with hash constructed from batch of messages
+const unichatAddress = "0x47a1fc3ef4d00e9862724e4d2cbc96a35e26a803"; // TODO: update this to other networks as needed
+
+// The ERC-20 Contract ABI, which is a common contract interface
+// for tokens (this is the Human-Readable ABI format)
+const unichatAbi = [
+  // human-readable ABI for Unichat contract
+  "function AddData(string memory _oldHash, string memory _newHash) payable public returns(bool)",
+  
+  "function WithdrawFunds(address payable _address) external returns(bool)",
+
+  "event NewEntry(string _hash)",
+  
+];
+
+const provider = new ethers.providers.JsonRpcProvider();
+
+const signer = provider.getSigner()
+
+
+// The Contract object
+const unichatContract = new ethers.Contract(unichatAddress, unichatAbi, provider);
+
+const invokeAddData = (hash: string) => {
+
+    const unichatWithSigner = unichatContract.connect(signer);
+
+    const eth = ethers.utils.parseUnits("1.0", 18);
+
+    const tx = unichatWithSigner.AddData("initial", "newhash");
+
+    return {
+        hash
+    }
+}
+
